9a7b4a8896e74ccd6b625ae43fdbce2e7514018a
sivasankaran <gnusankar@gmail.com>
2025-10-01 21:13:59 +0530

    done somthing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,28 @@
 
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('projectname');
+  loading = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+        setTimeout(() => this.loading = false, 2000);
+      }
+      if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        // If navigation ends before 2s, still hide loader
+        setTimeout(() => this.loading = false, 200);
+      }
+    });
+  }
 }
